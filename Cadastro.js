17ba1146adaf98f283f331372dbd8f786e1c290a
lucasvlb <lucas.villalba97@outlook.com>
2025-05-27 22:32:07 -0300

    Cadastro
    Cadastro

--- Cadastro.js
+++ Cadastro.js
@@ -1,36 +1,48 @@
+// fazer o cadastro
 const botao = document.getElementById('btnCadastrar');
-const listaUsuariosCadastrados = [];
+//const listaUsuariosCadastrados = [];
 
-
-//cadastrar
-botao.addEventListener('click', function() {
+// cadastrar
+botao.addEventListener('click', function (){
+    const listaUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
     const objUsuario = {
         usuario: document.getElementById('usuario').value,
         senha: document.getElementById('senha').value
     };
-    listaUsuariosCadastrados.push(objUsuario);
-    const listaJson = JSON.stringify(listaUsuariosCadastrados);
+    listaUsuarios.push(objUsuario);
+    const listaJson = JSON.stringify(listaUsuarios);
     localStorage.setItem('usuarios', listaJson);
     listar();
-});
+}
+
+);
 
 //listar
 function listar(){
     const listaUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
     let tabela = document.getElementById('listaUsuariosCadastrados');
-tabela.innerHTML = "";
+    tabela.innerHTML = "";
     listaUsuarios.forEach((objeto, index) => {
         let linha = document.createElement('tr');
         linha.innerHTML = `
-        <td>${objeto.usuario}</td>
-        <td>${objeto.senha}</td>
-        <td>
-        <button>Editar</button>
-        <button>Remover</button>
-        </td>
+            <td>${objeto.usuario}</td>
+            <td>${objeto.senha}</td>
+            <td>
+                <button>Editar</button>
+                <button>Remover</button>
+            </td>
         `;
-    tabela.appendChild(linha);
+        tabela.appendChild(linha);
     });
 }
+
+function removerUsuario(index){
+    const listaUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    if(confirm("Você realmente quer remover?")){
+    listaUsuarios.splice(index,1);
+    let listaJson = JSON.stringify(listaUsuarios);
+    localStorage.setItem("usuarios", listaJson);
+    listar();
+}}
 listar();
 
